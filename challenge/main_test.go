d04e54a90152fbb659f1f0387aba23cd8b5ee39c
kd14 <kd14@ford.com>
2023-09-17 22:22:18 +0530

    test_cases_waitgrps

--- challenge/main_test.go
+++ challenge/main_test.go
@@ -1,15 +1,37 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
 
 func TestMessage(t *testing.T) {
-
-	msg := "Kamalesh"
-
+	wg.Add(1)
+		go updateMessage("Kamalesh")
+		defer wg.Done()
+	wg.Wait()
 	if msg != "Kamalesh" {
-		t.Errorf("got %q, wanted %q", msg, "Kamalesh")
-
+		t.Error("Expected Kamalesh, got ", msg)
 	}
+}
+
+func TestPrintMessage(t *testing.T) {
+	stdOut := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	msg = "Kamalesh"
+	printMessage()
+	_ = w.Close()
+	result, _ := io.ReadAll(r)
+	output := string(result)
+	os.Stdout = stdOut
+
+	if !strings.Contains(output, "Kamalesh") {
+		t.Error("Expected to find Kamalesh")
+	}
+
 }
 
 func TestMain(t *testing.T) {
